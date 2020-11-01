ae03767e275543c3b5c895d1089b55cb2ffe58e2
nadyaoktvna <63992512+nadyaoktvna@users.noreply.github.com>
2020-11-01 15:23:54 +0800

    Update login dan daftar

---
+++ finddecor/daftar.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta content="width=device-width, initial-scale=1" name="viewport">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style-log.css">
+
+        <title>Daftar Akun</title>
+    </head>
+	<body>
+        <header>
+            <a class="logo title">findDécor</a>
+        </header>
+		<div class="container">
+			<div class="row justify-content-md-center mt-12">
+				<div class="col-sm-12">
+					<div class="row border-box">
+						<div class="col-6 p-3" align="center">
+                            <br /><br /><br /><br />
+                            <img src="assets/img/svg/undraw_Access_account_re_8spm.svg"  width="80%">	
+						</div>
+						<div class="col-sm-6 p-0">
+							<div class="shadow-lg p-2 bg-white rounded">
+								<div class="card">
+									<div class="text-center">
+                                        <br/><h2><b>DAFTAR</b></h2>
+									</div>
+									<div class="card-body p-3">
+										<form method="post">
+                                            <div class="form-group">
+                                                <label for="input_email" class="label">Email</label>
+                                                <input type="email" class="form-control form " id="input_email" >
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="input_nomor" class="label">Nomor Telepon</label>
+                                                <input type="number" class="form-control form " id="input_nomor" >
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="input_username" class="label">Username</label>
+                                                <input type="text" class="form-control form " id="input_username" >
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="input_password"class="label">Password</label>
+                                                <input type="password" class="form-control form" id="input_password" >
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="input_konfirpassword"class="label">Konfirmasi Password</label>
+                                                <input type="password" class="form-control form" id="input_konfirpassword" >
+                                                <br />
+                                            </div>
+                                                <a type="button" class="btn btn-secondary btn-lg btn-block masuk" href="#">
+                                                    DAFTAR
+                                                </a>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+		<footer>
+			<p>&copy; 2020, Kelompok 5</p>
+		</footer>
+	</body>
+</html>
---
+++ finddecor/login.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style-log.css">
+        <link rel="stylesheet" href="assets/fontawesome-free/css/all.min.css">
+        <title>Login</title>
+	</head>
+	<body>
+		<header>
+			<a class="logo title">findDécor</a>
+		</header>
+		<div class="container">
+			<div class="row justify-content-md-center mt-12">
+				<div class="col-sm-14">
+					<div class="row border-box">
+						<div class="col-6 p-3" align="center">
+							<br><br>
+							<img src="assets/img/svg/undraw_mobile_login_ikmv.svg" width="100%">
+						</div>
+						<div class="col-sm-6 p-5">
+							<div class="shadow-lg p-1 mb-0 bg-white rounded">
+								<div class="card">
+									<div class="text-center">
+										<br><h2><b>LOGIN</b></h2><br>
+									</div>
+									<br/>
+									<div class="card-body p-3">
+										<form method="post">
+                                            <div class="form-group">
+                                                <label for="input_email" class="label">Email</label>
+                                                <input type="email" class="form-control form " id="input_email" >
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="input_password"class="label">Password</label>
+                                                    <input type="password" class="form-control form" id="input_password" >
+                                                    <br /><br />
+                                            </div>
+                                                <a type="button" class="btn btn-secondary btn-lg btn-block masuk" href="#">
+                                                    LOGIN</a>
+                                              <br />
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+		<footer>
+			<p>&copy; 2020, Kelompok 5</p>
+		</footer>
+	</body>
+</html>
